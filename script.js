c01a0d8a9321cdb46e06ca342a26eab045aec2e9
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-04 10:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-
 //your JS code here. If required.
-let x=prompt('Enter a string');
-for(let i=0;i<x.length;i++){
-	if(x.charAt(i)==x.charAt(i+1)){
-		alert(x.charAt(i-1));
+
+function nonRepeating(str){
+	const char={};
+	//count frequency
+	for(let i=;i<str.length;i++){
+		const c=str[i];
+		char[c]=char[c]?char[c]+1:1;
 	}
+
+	for(let i=0;i<str.length;i++){
+		const chara=str[i];
+		if(char[chara] === 1){
+			return chara;
+		}
+	}
+	return null;
+}
+const input=prompt("Enter a string");
+const firstnonrepeat=nonRepeating(input);
+
+if(firstnonrepeat){
+	alert ("$[firstnonrepeat]");
+}
+else{
+	alert("there is no repeated character");
 }
